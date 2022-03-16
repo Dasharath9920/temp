1c609ca6727e1f9a9da565d15f3ca444fa308570
Dasharath9920 <dyavaridasharath82@gmail.com>
2022-03-16 15:46:48 +0530

    new version 1.2

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <button onClick={() => console.log("clicked")}>submit</button>
       <h1>This is Dasharath</h1>
       <p>This is an extra line here</p>
+      <p>This is one more extra line</p>
     </div>
   );
 }
