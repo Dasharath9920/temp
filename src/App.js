b9bf10fd005bcddad0c3a51115a1927210af5da4
Dasharath9920 <dyavaridasharath82@gmail.com>
2022-03-16 15:39:23 +0530

    new version added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button onClick={() => console.log("clicked")}>submit</button>
+      <h1>This is Dasharath</h1>
+      <p>This is an extra line here</p>
+    </div>
+  );
+}
+
+export default App;
