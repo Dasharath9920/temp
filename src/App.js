aac6a76c1c24fdbed3955cf9a607b29e9f633477
Dasharath938 <dasharath.dyavari@interviewvector.tech>
2022-03-21 19:36:52 +0530

    made some changes
    made some changes and modified some files

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <button onClick={() => console.log("clicked")}>submit</button>
-      <h1>This is Dasharath</h1>
-      <p>This is an extra line here</p>
-      <p>This is one more extra line</p>
+      <h1>Hello world</h1>
     </div>
   );
 }
